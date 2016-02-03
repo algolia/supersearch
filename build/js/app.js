89d45abcea7927cded00ebd516b2e1a107b98933
Shipow <shipowlata@gmail.com>
2016-02-03 21:28:55 +0100

    Video : close with escape + fullscreen

--- build/js/app.js
+++ build/js/app.js
@@ -2705,7 +2705,7 @@
 	$('#results').on('click touchstart', '.hit', function (e) {
 	  e.preventDefault();
 	  var yt = $(this).data('id');
-	  $('.lightbox_frame_wrapper').append($('<iframe class="lightbox_frame" type="text/html" width="640" height="385"></iframe>'));
+	  $('.lightbox_frame_wrapper').append($('<iframe class="lightbox_frame" type="text/html" width="640" height="385" allowfullscreen></iframe>'));
 	  $('.lightbox_frame').attr('src', 'http://www.youtube.com/embed/' + yt + '?autoplay=1').load(function () {
 	    $(this).addClass('loaded');
 	  });
@@ -2714,12 +2714,23 @@
 	  $('.container-fluid').addClass('no-scroll');
 	});
 
-	var $lightbox = $('<div class="lightbox hidden"><a href="javascript:void(0)" class="close"><svg height="34px" id="Layer_1" style="enable-background:new 0 0 34 34;" version="1.1" viewBox="0 0 512 512" width="34px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z"/></svg></a><div class="lightbox_frame_wrapper"></div>').on('click touchstart', '.close', function () {
+	$(document).keyup(function (e) {
+	  if (e.keyCode == 27 && $('.lightbox:not(.hidden)') != null) {
+	    closeLightbox();
+	  }
+	});
+
+	function closeLightbox() {
 	  $('.lightbox_frame').remove();
 	  $('.lightbox').toggleClass('hidden');
 	  $('body, html').css('overflow:auto');
 	  $('.container-fluid').removeClass('no-scroll');
+	}
+
+	var $lightbox = $('<div class="lightbox hidden"><a href="javascript:void(0)" class="close"><svg height="34px" id="Layer_1" style="enable-background:new 0 0 34 34;" version="1.1" viewBox="0 0 512 512" width="34px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z"/></svg></a><div class="lightbox_frame_wrapper"></div>').on('click touchstart', '.close', function () {
+	  closeLightbox();
 	});
+
 	$('body').append($lightbox);
 
 	$('.sbx-custom__reset').on('click touchstart', function (e) {
