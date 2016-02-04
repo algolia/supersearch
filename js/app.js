b2fa109a3ab9665433a4c8c0514d5561c9ede918
Shipow <shipowlata@gmail.com>
2016-02-04 02:17:14 +0100

    Update 2016-02-04T01:17:13.332Z

--- js/app.js
+++ js/app.js
@@ -50,6 +50,9 @@
 	__webpack_require__(3);
 	__webpack_require__(6);
 
+	__webpack_require__(8);
+	__webpack_require__(9);
+
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
@@ -12614,6 +12617,53 @@
 	return jQuery;
 	}));
 
+
+/***/ },
+/* 8 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	(function (i, s, o, g, r, a, m) {
+	  i['GoogleAnalyticsObject'] = r;i[r] = i[r] || function () {
+	    (i[r].q = i[r].q || []).push(arguments);
+	  }, i[r].l = 1 * new Date();a = s.createElement(o), m = s.getElementsByTagName(o)[0];a.async = 1;a.src = g;m.parentNode.insertBefore(a, m);
+	})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+	ga('create', 'UA-32446386-14', 'auto');
+	ga('send', 'pageview');
+
+/***/ },
+/* 9 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	adroll_adv_id = "TGR7R4XSGVGYVPX4LQBDFQ";
+	adroll_pix_id = "YQNWNHGUUBD2JMY7BVOBWM";
+	/* OPTIONAL: provide email to improve user identification */
+	/* adroll_email = "username@example.com"; */
+	(function () {
+	    var _onload = function _onload() {
+	        if (document.readyState && !/loaded|complete/.test(document.readyState)) {
+	            setTimeout(_onload, 10);return;
+	        }
+	        if (!window.__adroll_loaded) {
+	            __adroll_loaded = true;setTimeout(_onload, 50);return;
+	        }
+	        var scr = document.createElement("script");
+	        var host = "https:" == document.location.protocol ? "https://s.adroll.com" : "http://a.adroll.com";
+	        scr.setAttribute('async', 'true');
+	        scr.type = "text/javascript";
+	        scr.src = host + "/j/roundtrip.js";
+	        ((document.getElementsByTagName('head') || [null])[0] || document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+	    };
+	    if (window.addEventListener) {
+	        window.addEventListener('load', _onload, false);
+	    } else {
+	        window.attachEvent('onload', _onload);
+	    }
+	})();
 
 /***/ }
 /******/ ]);
