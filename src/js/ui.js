701068a57a474f33015990d530a976c941298888
Shipow <shipowlata@gmail.com>
2016-02-04 02:01:20 +0100

    Share + fixes

--- src/js/ui.js
+++ src/js/ui.js
@@ -58,3 +58,17 @@ $('.tabs a').on('click touchstart', function(e){
   $('.tabs li a').toggleClass('active');
   $('.tab-panel').toggleClass('active');
 });
+
+$(".share-link").on("click", function(e) {
+  e.preventDefault();
+  var e = "http://shipow.github.io/superbowl/"
+    , n = "Never struggle to find a Super Bowl ad again. Find the good, the bad and the @Nationwide.";
+  switch (this.href.split("#")[1]) {
+  case "twitter":
+    window.open("https://twitter.com/share?url=" + encodeURIComponent(e) + "&text=" + encodeURIComponent(n) + "&via=algolia", "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600");
+    break;
+  case "facebook":
+    window.open("https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(e) + "&t=" + encodeURIComponent(n), "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600");
+    break;
+  }
+});
