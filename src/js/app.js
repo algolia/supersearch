ae8cfac272911a86871c56501730202fe4e3a288
Shipow <shipowlata@gmail.com>
2016-02-05 15:20:49 +0100

    Share videos

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@
 
 require('./superbowl_search.js');
 require('./jquery-1.12.0.min.js');
+require('./jhash-2.1.js');
+
 require('./ui.js');
 
 require('./analytics.js');
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,26 +1,41 @@
-$('#results').on('click', '.hit', function(e){
-  e.preventDefault();
-  var yt = $(this).data('id');
-  $('.lightbox_frame_wrapper').append($('<iframe class="lightbox_frame" type="text/html" width="640" height="385" allowfullscreen></iframe>'));
+function openLightbox(e) {
+  jHash.val('yt', $(e).data('yt'));
+  $('.lightbox_frame_wrapper')
+    .append($('<iframe class="lightbox_frame" type="text/html" width="640" height="385" allowfullscreen></iframe>'));
   $('.lightbox_frame').attr('src', 'https://www.youtube.com/embed/'+ yt + '?autoplay=1').load(function(){
     $(this).addClass('loaded');
   });
   $('.lightbox').toggleClass('hidden');
   $('body, html').css('overflow:hidden');
   $('.container-fluid').addClass('no-scroll');
-});
+  $(".lightbox .share-video").data('brand', $(e).data('brand'));
+};
 
 function closeLightbox() {
   $('.lightbox_frame').remove();
   $('.lightbox').addClass('hidden');
   $('body, html').css('overflow:auto');
   $('.container-fluid').removeClass('no-scroll');
-}
+  jHash.clearQuery();
+};
 
-var $lightbox = $('<div class="lightbox hidden"><a href="javascript:void(0)" class="close"><svg height="34px" id="Layer_1" style="enable-background:new 0 0 34 34;" version="1.1" viewBox="0 0 512 512" width="34px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z"/></svg></a><div class="lightbox_frame_wrapper"></div>');
+// init
+var yt = jHash.val('yt');
+if (yt) {
+  openLightbox(yt);
+};
 
-$lightbox.on('click', function(){
+$('#results').on('click', '.hit', function(e) {
+  e.preventDefault();
+  var yt = $(this).data('yt');
+  var brand = $(this).data('brand');
+  openLightbox(e.target);
+});
+
+$('.lightbox').on('click', function(){
   closeLightbox();
+}).find('.lightbox_sharing').click(function(e) {
+    return false;
 });
 
 $(document).keyup(function(e) {
@@ -28,8 +43,6 @@ $(document).keyup(function(e) {
     closeLightbox();
   }
 });
-
-$('body').append($lightbox);
 
 $('.sbx-custom__reset').on('click touchstart', function(e) {
   e.preventDefault();
@@ -71,4 +84,20 @@ $(".share-link").on("click", function(e) {
     window.open("https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(e) + "&t=" + encodeURIComponent(n), "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600");
     break;
   }
+});
+
+$(".share-video").on("click", function(e) {
+  e.preventDefault();
+  var yt = jHash.val('yt');
+  var brand = $(this).data('brand');
+  var e = "https://community.algolia.com/supersearch/#?yt=" + yt
+    , n = "I ❤ this #SuperBowl #ad by " + brand + " on 🏈 SuperSearch Commercials 🏈 ";
+    switch (this.href.split("#")[1]) {
+    case "twitter":
+      window.open("https://twitter.com/share?url=" + encodeURIComponent(e) + "&text=" + encodeURIComponent(n) + "&via=algolia", "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600");
+      break;
+    case "facebook":
+      window.open("https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(e) + "&t=" + encodeURIComponent(n), "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600");
+      break;
+    }
 });
