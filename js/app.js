1d3710efce3d7eadfae5bd0575941a82b994bd03
Shipow <shipowlata@gmail.com>
2016-02-05 15:21:46 +0100

    Update 2016-02-05T14:21:44.386Z

--- js/app.js
+++ js/app.js
@@ -50,8 +50,10 @@
 	__webpack_require__(3);
 	__webpack_require__(6);
 
-	__webpack_require__(8);
+	__webpack_require__(7);
+
 	__webpack_require__(9);
+	__webpack_require__(10);
 
 /***/ },
 /* 1 */
@@ -2701,13 +2703,296 @@
 
 /***/ },
 /* 6 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+	/*
+	* jHash v2.1
+	* http://jhash.codeplex.com
+	* 
+	* Copyright (c) 2013 Chris Pietschmann
+	* 
+	* Permission is hereby granted, free of charge, to any person obtaining a copy of 
+	* this software and associated documentation files (the "Software"), to deal in the 
+	* Software without restriction, including without limitation the rights to use, copy, 
+	* modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+	* and to permit persons to whom the Software is furnished to do so, subject to the 
+	* following conditions:
+	* 
+	* The above copyright notice and this permission notice shall be included in all copies 
+	* or substantial portions of the Software.
+	* 
+	* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+	* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+	* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+	* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+	* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	*/
+	(function (window) {
+	    var ie_documentMode = window.document.documentMode;
+	    var hashChangeSupported = 'onhashchange' in window && (ie_documentMode === undefined || ie_documentMode > 7);
+	    var jHash = window.jHash = {
+	        jhash: "2.1",
+
+	        /// adds an event handler for the "hashchange" event
+	        change: function change(handler) {
+	            if (hashChangeSupported) {
+	                attachEvent(window, "hashchange", handler);
+	            } else {
+	                eventHandlers.push(handler);
+	            }
+	        },
+
+	        /// removes an event handler from the "hashchange" event
+	        unbind: function unbind(handler) {
+	            var i = 0,
+	                len = 0;
+	            if (hashChangeSupported) {
+	                detachEvent(window, "hashchange", handler);
+	            } else {
+	                arrayRemove(eventHandlers, eventHandlers.indexOf(handler));
+	            }
+	            return this;
+	        },
+
+	        /// gets or sets a hash querystring value
+	        val: function val(name, value) {
+	            var ho = jHash.query();
+	            if (arguments.length === 2) {
+	                ho[name.toLowerCase()] = value === null ? '' : value;
+	                return this.set(this.root(), ho);
+	            } else if (arguments.length === 1 && typeof name === 'string') {
+	                return ho[name.toLowerCase()];
+	            } else if ((typeof name === "undefined" ? "undefined" : _typeof(name)) === 'object') {
+	                return this.set(this.root(), name);
+	            }
+	            return ho;
+	        },
+
+	        /// gets or sets the root hash
+	        root: function root(value) {
+	            if (value === undefined) {
+	                return parseHashRoot(window.location.hash);
+	            }
+	            return this.set(value, this.val());
+	        },
+
+	        /// sets both the root hash and the hash querystring
+	        set: function set(root, query) {
+	            var fullHashValue = null;
+	            if (root === null && query === null) {
+	                fullHashValue = null;
+	            } else if (query !== undefined) {
+	                var rootValue = root;
+	                var queryValue = typeof query === "string" ? query : objectToHash(query);
+	                if (queryValue.length > 0) {
+	                    rootValue += '?';
+	                }
+	                fullHashValue = rootValue + queryValue;
+	            } else {
+	                fullHashValue = root;
+	            }
+	            window.location.hash = fullHashValue;
+	            return this;
+	        },
+
+	        /// removes a value from the hash querystring
+	        remove: function remove(name) {
+	            var ho = jHash.query();
+	            ho[name.toLowerCase()] = undefined;
+	            return this.set(this.root(), ho);
+	        },
+
+	        /// returns an object representation of the hash querystring
+	        query: function query() {
+	            return hashToObject(window.location.hash);
+	        },
+
+	        /// clears the hash
+	        clear: function clear() {
+	            window.location.hash = '';
+	            return this;
+	        },
+	        // clears the hash querystring
+	        clearQuery: function clearQuery() {
+	            this.set(this.root());
+	            return this;
+	        },
+	        // clears the hash root
+	        clearRoot: function clearRoot() {
+	            this.set('', this.query());
+	            return this;
+	        },
+
+	        _routes: {},
+	        _routeHandlerRegistered: false,
+	        _defaultRoute: null,
+
+	        /// adds a new route handler
+	        route: function route(_route, handler) {
+	            this._routes[_route] = handler;
+
+	            if (!this._routeHandlerRegistered) {
+	                this._routeHandlerRegistered = true;
+	                jHash.change(routeHandler);
+	            }
+	        },
+
+	        /// Forces the current route to be processed.
+	        /// useful for calling on initial page load, after
+	        /// all page initialization has been performed
+	        processRoute: function processRoute() {
+	            routeHandler();
+	        },
+
+	        /// Gets or sets the default route for the page, when
+	        /// no "root" hash is specified
+	        defaultRoute: function defaultRoute(root, query) {
+	            if (arguments.length == 0) {
+	                return this._defaultRoute;
+	            }
+	            this._defaultRoute = {
+	                root: root,
+	                query: query
+	            };
+	        }
+	    };
+
+	    if (!hashChangeSupported) {
+	        window.setInterval(function () {
+	            var currentHash = window.location.hash;
+	            if (previousHashValue !== currentHash) {
+	                for (var i in eventHandlers) {
+	                    eventHandlers[i].call(window);
+	                }
+	            }
+	            previousHashValue = currentHash;
+	        }, 200);
+	    }
+
+	    var eventHandlers = [],
+	        previousHashValue = window.location.hash,
+	        attachEvent = function attachEvent(element, evtName, handler) {
+	        if (element.addEventListener) {
+	            element.addEventListener(evtName, handler, false);
+	        } else if (element.attachEvent) {
+	            element.attachEvent("on" + evtName, handler);
+	        } else {
+	            element["on" + evtName] += handler;
+	        }
+	    },
+	        detachEvent = function detachEvent(element, evtName, handler) {
+	        if (element.removeEventListener) {
+	            element.removeEventListener(evtName, handler, false);
+	        } else if (element.detachEvent) {
+	            element.detachEvent("on" + evtName, handler);
+	        } else {
+	            element["on" + evtName] -= handler;
+	        }
+	    },
+	        arrayRemove = function arrayRemove(array, from, to) {
+	        /* function source: http://ejohn.org/blog/javascript-array-remove/ */
+	        var rest = array.slice((to || from) + 1 || array.length);
+	        array.length = from < 0 ? array.length + from : from;
+	        return array.push.apply(array, rest);
+	    },
+	        hashToObject = function hashToObject(hash) {
+	        /* create a "dictionary" object for the passed in hash value */
+	        var obj = {},
+	            pair = null,
+	            strHash = hash.substring(0, hash.length);
+	        if (hash !== null && hash !== undefined) {
+	            if (strHash.indexOf("#") === 0) {
+	                strHash = strHash.substring(1, strHash.length);
+	            }
+
+	            var queryIndex = strHash.indexOf("?");
+
+	            if (queryIndex > -1) {
+	                strHash = strHash.substring(queryIndex + 1, strHash.length);
+
+	                var parts = strHash.split("&");
+	                for (var i in parts) {
+	                    pair = parts[i].split("=");
+	                    obj[pair[0].toString().toLowerCase()] = pair[1];
+	                }
+	            }
+	        }
+	        return obj;
+	    },
+	        objectToHash = function objectToHash(object) {
+	        var s = "";
+	        for (var i in object) {
+	            if (object[i] !== undefined) {
+	                if (s.length > 0) {
+	                    s += "&";
+	                }
+	                s += i.toString() + "=" + object[i].toString();
+	            }
+	        }
+	        return s;
+	    },
+	        parseHashRoot = function parseHashRoot(hash) {
+	        var strHash = hash.substring(0, hash.length);
+	        if (strHash.indexOf("#") > -1) {
+	            strHash = strHash.substring(1, strHash.length);
+	        }
+	        if (strHash.indexOf("?") > -1) {
+	            strHash = strHash.substring(0, strHash.indexOf("?"));
+	        }
+	        return strHash;
+	    },
+	        ROUTE_REPLACE = "([^\/]+)",
+	        ROUTE_MATCH = /{([\w\d]+)}/g,
+	        getRouteMatches = function getRouteMatches(route, root) {
+	        var pathRegex = new RegExp(route.replace(ROUTE_MATCH, ROUTE_REPLACE) + "$");
+	        return root.match(pathRegex);
+	    },
+	        routeHandler = function routeHandler() {
+	        var root = jHash.root();
+
+	        var defaultRoute = jHash.defaultRoute();
+	        if ((root || '').length === 0 && defaultRoute) {
+	            jHash.set(defaultRoute.root, defaultRoute.query);
+	            return;
+	        }
+
+	        for (var r in jHash._routes) {
+	            if (typeof r === "string") {
+	                var matches = getRouteMatches(r, root);
+	                if (matches !== null) {
+	                    var handler = jHash._routes[r];
+
+	                    var context = {};
+	                    var routeParts = r.match(ROUTE_MATCH);
+	                    if (routeParts !== null) {
+	                        for (var rp = 0; rp < routeParts.length; rp++) {
+	                            var routePartName = routeParts[rp].substring(1).substring(0, routeParts[rp].length - 2);
+	                            context[routePartName] = matches[rp + 1];
+	                        }
+	                    }
+
+	                    handler.call(context);
+
+	                    return;
+	                }
+	            }
+	        }
+	    };
+	})(window);
+
+/***/ },
+/* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function($) {'use strict';
 
-	$('#results').on('click', '.hit', function (e) {
-	  e.preventDefault();
-	  var yt = $(this).data('id');
+	function openLightbox(e) {
+	  jHash.val('yt', $(e).data('yt'));
 	  $('.lightbox_frame_wrapper').append($('<iframe class="lightbox_frame" type="text/html" width="640" height="385" allowfullscreen></iframe>'));
 	  $('.lightbox_frame').attr('src', 'https://www.youtube.com/embed/' + yt + '?autoplay=1').load(function () {
 	    $(this).addClass('loaded');
@@ -2715,19 +3000,34 @@
 	  $('.lightbox').toggleClass('hidden');
 	  $('body, html').css('overflow:hidden');
 	  $('.container-fluid').addClass('no-scroll');
-	});
+	  $(".lightbox .share-video").data('brand', $(e).data('brand'));
+	};
 
 	function closeLightbox() {
 	  $('.lightbox_frame').remove();
 	  $('.lightbox').addClass('hidden');
 	  $('body, html').css('overflow:auto');
 	  $('.container-fluid').removeClass('no-scroll');
-	}
+	  jHash.clearQuery();
+	};
 
-	var $lightbox = $('<div class="lightbox hidden"><a href="javascript:void(0)" class="close"><svg height="34px" id="Layer_1" style="enable-background:new 0 0 34 34;" version="1.1" viewBox="0 0 512 512" width="34px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z"/></svg></a><div class="lightbox_frame_wrapper"></div>');
+	// init
+	var yt = jHash.val('yt');
+	if (yt) {
+	  openLightbox(yt);
+	};
 
-	$lightbox.on('click', function () {
+	$('#results').on('click', '.hit', function (e) {
+	  e.preventDefault();
+	  var yt = $(this).data('yt');
+	  var brand = $(this).data('brand');
+	  openLightbox(e.target);
+	});
+
+	$('.lightbox').on('click', function () {
 	  closeLightbox();
+	}).find('.lightbox_sharing').click(function (e) {
+	  return false;
 	});
 
 	$(document).keyup(function (e) {
@@ -2735,8 +3035,6 @@
 	    closeLightbox();
 	  }
 	});
-
-	$('body').append($lightbox);
 
 	$('.sbx-custom__reset').on('click touchstart', function (e) {
 	  e.preventDefault();
@@ -2779,10 +3077,26 @@
 	      break;
 	  }
 	});
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(7)))
+
+	$(".share-video").on("click", function (e) {
+	  e.preventDefault();
+	  var yt = jHash.val('yt');
+	  var brand = $(this).data('brand');
+	  var e = "https://community.algolia.com/supersearch/#?yt=" + yt,
+	      n = "I ❤ this #SuperBowl #ad by " + brand + " on 🏈 SuperSearch Commercials 🏈 ";
+	  switch (this.href.split("#")[1]) {
+	    case "twitter":
+	      window.open("https://twitter.com/share?url=" + encodeURIComponent(e) + "&text=" + encodeURIComponent(n) + "&via=algolia", "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600");
+	      break;
+	    case "facebook":
+	      window.open("https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(e) + "&t=" + encodeURIComponent(n), "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600");
+	      break;
+	  }
+	});
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(8)))
 
 /***/ },
-/* 7 */
+/* 8 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -12619,7 +12933,7 @@
 
 
 /***/ },
-/* 8 */
+/* 9 */
 /***/ function(module, exports) {
 
 	'use strict';
@@ -12634,7 +12948,7 @@
 	ga('send', 'pageview');
 
 /***/ },
-/* 9 */
+/* 10 */
 /***/ function(module, exports) {
 
 	"use strict";
